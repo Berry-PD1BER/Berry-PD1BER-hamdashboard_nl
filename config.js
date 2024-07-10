d5959aa25671c752494d01718e25d8d998c0d3b7
Berry-PD1BER <pd1ber@hotmail.com>
2024-07-10 13:18:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -55,10 +55,10 @@ const aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 const aIMG = [
-  ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
+  ["NL REGENRADAR", "https://widgets.meteox.com/nl-NL/widgets/radar/country/nl/rain"],
   [
-    "LOCAL RADAR",
-    "https://radar.weather.gov/ridge/standard/KNQA_loop.gif",
+    "NL SAT BEWOLKING",
+    "https://widgets.meteox.com/nl-NL/widgets/radar/country/nl/satellite",
   ],
   [
     "NOAA D-RAP",
